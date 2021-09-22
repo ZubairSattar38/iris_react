7b2e33f224a221a9ce6f120e6c2275f8106fee9d
Zubair Sattar <zubairsattar38@gmail.com>
2021-09-22 22:31:55 +0500

    Updated Review Responsive

--- src/screens/Testimonials/Reviews.js
+++ src/screens/Testimonials/Reviews.js
@@ -1,70 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import { COLORS } from '../../GlobalVariables';
+import { useMediaQuery } from 'react-responsive';
 
 function Reviews(props) {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(max-width: 1100px)'
+    })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 820px)' })
+
+
+    const styles = {
+        image: {
+            width: '25%',
+            marginTop: '-8%',
+            position: 'relative',
+        },
+        review: {
+            display: 'flex',
+            backgroundColor: COLORS.black,
+            flexDirection: isTabletOrMobile ? 'column' : isDesktopOrLaptop ? 'column' : 'row',
+            margin: isTabletOrMobile ? '4%' : isDesktopOrLaptop ? '4%' : '',
+
+            width: !isTabletOrMobile&&!isDesktopOrLaptop&&'70%',
+            borderRadius: 6,
+            justifyContent: 'center',
+        },
+        reviewLeftSide: {
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            width: '50%',
+            paddingLeft: 20,
+            color: COLORS.white,
+
+        },
+        review_Wrapper: {
+            display: 'flex',
+            // flexDirection: isTabletOrMobile ? 'column' : isDesktopOrLaptop ? 'column' : 'row',
+            justifyContent: 'center',
+            marginBottom: '3%'
+        },
+        multiButton: {
+            flex: 1.5,
+            paddingTop: "5%",
+            alignSelf: 'end',
+            textAlign: 'justify'
+
+        },
+        desc: {
+            fontSize: '0.8rem',
+            width: '80%',
+            paddingLeft: '3%',
+            color: COLORS.white
+        }
+    }
     return (
         <div style={styles.review_Wrapper}>
-        <div style={styles.review}>
+            <div style={styles.review}>
 
-            <div style={styles.reviewLeftSide}>
-                <img style={styles.image} src={props.image}/>
+                <div style={styles.reviewLeftSide}>
+                    <img style={styles.image} src={props.image} />
                     <h1>{props.title}</h1>
-            </div>
-            <div style={styles.multiButton}>
-                <div style={styles.desc}>
-                    <img src="https://codingpixel.com/wp-content/themes/cp-theme/images/icons/inverted-comma.svg"/>
-                    <p>{props.desc}</p>
+                </div>
+                <div style={styles.multiButton}>
+                    <div style={styles.desc}>
+                        <img src="https://codingpixel.com/wp-content/themes/cp-theme/images/icons/inverted-comma.svg" />
+                        <p>{props.desc}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
-const styles = {
-    image:{
-        width:'25%',
-        marginTop:'-20%',
-        position: 'relative',
 
-        // paddingLeft:'30%'
-    },
-    review: {
-        display: 'flex',
-        backgroundColor: COLORS.black,
-        width: '70%',
-        borderRadius:6,
-
-        justifyContent: 'center',
-    },
-    reviewLeftSide:{
-        // backgroundImage: linearGradient(to right, 'black' '50%', 'blue' '50%'),
-        flex: 1,
-        display: 'flex',
-        flexDirection:'column',
-        textAlign: "left",
-        alignSelf:'center',
-        width:'50%',
-        paddingLeft:20,
-        color: COLORS.white,
-
-    },
-    review_Wrapper: {
-        display: 'flex',
-        justifyContent: 'center',
-        marginBottom:'3%'
-    },
-    multiButton: {
-        flex: 1.5,
-        paddingTop:"5%",
-        alignSelf: 'end',
-        textAlign: 'justify'
-        
-    },
-    desc: {
-        fontSize: '0.8rem',
-        width: '70%',
-        paddingLeft: '10%',
-        color: COLORS.white
-    }
-}
 export default Reviews;
--- src/screens/Testimonials/WhatTheySay.js
+++ src/screens/Testimonials/WhatTheySay.js
@@ -1,14 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import { COLORS } from '../../GlobalVariables';
 import Reviews from './Reviews';
+import { useMediaQuery } from 'react-responsive';
+
 
 function WhatTheySay() {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(max-width: 1100px)'
+    })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 820px)' })
+    const styles = {
+        Review: {
+            background: `linear-gradient(to bottom, ${COLORS.midGrey} 0%,  ${COLORS.midGrey} 50%, ${COLORS.white} 50%, ${COLORS.white} 100%)`,
+
+        },
+        WhatTheySay: {
+
+            display: 'flex',
+            background: COLORS.midGrey,
+            alignItems: isTabletOrMobile ? 'left' : isDesktopOrLaptop && 'left',
+            alignItems: !isTabletOrMobile && !isDesktopOrLaptop && 'center',
+            flexDirection: isTabletOrMobile ? 'column' : isDesktopOrLaptop ? 'column' : 'row',
+            paddingLeft: '3%',
+            paddingBottom: '4%'
+            // height: '30vh'
+        },
+        ourService: {
+            fontSize: 15,
+            fontWeight: 800
+        },
+        leftSide: {
+            flex: 1,
+            paddingTop: '2%',
+            textAlign: isTabletOrMobile ? 'left' : isDesktopOrLaptop && 'left',
+            textAlign: !isTabletOrMobile && !isDesktopOrLaptop && 'center',
+        },
+        title: {
+            fontSize: isTabletOrMobile ? '2rem' : '3.5rem',
+            fontWeight: 800
+        },
+        multiButton: {
+            flex: 1.5,
+            paddingLeft: !isTabletOrMobile && !isDesktopOrLaptop && '10%',
+        },
+        desc: {
+            fontSize: '0.8rem',
+            width: '70%'
+        }
+    }
     return (
         <div>
             <div style={styles.WhatTheySay} >
                 <div style={styles.leftSide}>
                     <div style={styles.title}>
-                        <span>What <br /> &emsp; &emsp; They Say</span>
+                        <span>What <br /> &emsp; &nbsp; They Say</span>
                     </div>
                 </div>
                 <div style={styles.multiButton}>
@@ -25,38 +70,86 @@ function WhatTheySay() {
         </div>
     )
 }
-const styles = {
-    Review: {
-        background: `linear-gradient(to bottom, ${COLORS.midGrey} 0%,  ${COLORS.midGrey} 50%, ${COLORS.white} 50%, ${COLORS.white} 100%)`,
 
-    },
-    WhatTheySay: {
-        display: 'flex',
-        background: COLORS.midGrey,
-        alignItems: 'center',
-        paddingBottom: '4%'
-        // height: '30vh'
-    },
-    ourService: {
-        fontSize: 15,
-        fontWeight: 800
-    },
-    leftSide: {
-        flex: 1,
-        paddingTop: '2%',
-        textAlign: "center",
-    },
-    title: {
-        fontSize: '3.5rem',
-        fontWeight: 800
-    },
-    multiButton: {
-        flex: 1.5,
-        paddingLeft: '10%',
-    },
-    desc: {
-        fontSize: '0.8rem',
-        width: '70%'
-    }
-}
 export default WhatTheySay;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { COLORS } from '../../GlobalVariables';
+// import { useMediaQuery } from 'react-responsive';
+
+// function OurWork() {
+
+//     const isDesktopOrLaptop = useMediaQuery({
+//         query: '(max-width: 980px)'
+//     })
+//     const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+//     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 820px)' })
+
+//     const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+//     const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+
+//     const isLargeScaleTablet = useMediaQuery({ query: '(max-width: 1092px)' })
+//     const styles = {
+//         ourWork: {
+//             display: 'flex',
+//             background: COLORS.white,
+//             alignItems: isTabletOrMobile ?'left':isDesktopOrLaptop &&'left',
+//             alignItems: !isTabletOrMobile && !isDesktopOrLaptop &&'center',
+//             flexDirection: isTabletOrMobile ?'column':isDesktopOrLaptop?'column':'row',
+//             paddingLeft: '3%'
+//         },
+//         ourService: {
+//             fontSize: 15,
+//             fontWeight: 800
+//         },
+//         leftSide: {
+//             flex: 1,
+//             paddingTop: '2%',
+//         },
+//         title: {
+//             fontSize: isTabletOrMobile ? '2rem' : '3.5rem',
+//             fontWeight: 800
+//         },
+//         multiButton: {
+//             flex: 1.5,
+//             paddingLeft: '10%',
+//         },
+//         desc: {
+//             fontSize: '0.8rem',
+//             width: '70%'
+//         }
+//     }
+//     return (
+//         <div style={styles.ourWork} >
+//             <div style={styles.leftSide}>
+//                 <div style={styles.ourService}>
+//                     <p>OUR WORK</p>
+//                 </div>
+//                 <div style={styles.title}>
+//                     <span>We Love <br /> &emsp; &nbsp; Our Work</span>
+//                 </div>
+//             </div>
+//             <div style={styles.multiButton}>
+//                 <div style={styles.desc}>
+//                     <p>
+//                         With hundreds of projects completed for industry-leading clients, we’ve done almost every type of project. Many of which became award winners and made us proud.
+//                     </p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default OurWork;
